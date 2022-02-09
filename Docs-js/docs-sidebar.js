201ca62144be0436acc31d207a7209a121cc2334
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-02-09 18:23:28 +0530

    feat : added css of documentation

---
+++ Docs-js/docs-sidebar.js
@@ -0,0 +1,16 @@
+const sidebarHamburger = document.querySelector("#sidebar-hamburger");
+const minimizedSidebar = document.querySelector(".minimized-sidebar");
+
+sidebarHamburger.onclick = () => {
+  if (minimizedSidebar.style.display === "block") {
+    minimizedSidebar.style.display = "none";
+  } else {
+    minimizedSidebar.style.display = "block";
+  }
+};
+
+const closeSidebarBtn = document.querySelector(".sidebar-close");
+
+closeSidebarBtn.onclick = () => {
+  minimizedSidebar.style.display = "none";
+};
